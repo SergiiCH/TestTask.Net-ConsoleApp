2b671325178a1a7e0dd5f300cc983d2f8a69e218
SergiiCH <poshtovaskrinka.s2017@ukr.net>
2020-01-20 11:00:47 +0200

    Добавьте файлы проекта.

---
+++ Декорирование по ТЗ УПЦ/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Декорирование_по_ТЗ_УПЦ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
